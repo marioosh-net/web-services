f5a8d86e548a8ab2d188357078f3ad2a569f5029
marioosh <marioosh.net@gmail.com>
2012-03-14 15:56:12 +0100

    save

---
+++ m3_cw2/src/pl/szkolenie/service/UserRepositoryImpl.java
@@ -0,0 +1,39 @@
+package pl.szkolenie.service;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.jws.WebService;
+
+import com.sun.xml.internal.ws.developer.SchemaValidation;
+
+import pl.szkolenie.model.User;
+
+@SchemaValidation
+@WebService
+public class UserRepositoryImpl implements UserRepository {
+
+	private long lastId;
+	Map<Long, User> users = new HashMap<Long, User>();
+	
+	@Override
+	public long addUser(User user) {
+		users.put(++lastId, user);
+		return lastId;
+	}
+
+	@Override
+	public User getUser(long id) {
+		if(users.containsKey(id)) {
+			return users.get(id);
+		}
+		throw new IllegalArgumentException();
+	}
+
+	@Override
+	public Collection<User> getAll() {
+		return users.values();
+	}
+
+}
