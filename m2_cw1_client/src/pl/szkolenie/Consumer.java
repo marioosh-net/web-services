49550e3a808b9a7d6b3dde43e28cad88be0c37f5
marioosh <marioosh.net@gmail.com>
2012-03-14 11:27:44 +0100

    save

--- m2_cw1_client/src/pl/szkolenie/Consumer.java
+++ m2_cw1_client/src/pl/szkolenie/Consumer.java
@@ -1,5 +1,6 @@
 package pl.szkolenie;
 
+import pl.szkolenie.ws.User;
 import pl.szkolenie.ws.UserRepositoryImpl;
 import pl.szkolenie.ws.UserRepositoryImplService;
 
@@ -8,6 +9,14 @@ public class Consumer {
 		UserRepositoryImplService service = new UserRepositoryImplService();
 		UserRepositoryImpl usersRepository = service.getUserRepositoryImplPort();
 		
-		usersRepository.getAll()
+		long id1 = usersRepository.addUser(new User("Jan", "Kowalski"));
+		long id2 = usersRepository.addUser(new User("Adam", "Nowak"));
+		
+		System.out.println("user1: "+usersRepository.getUser(id1));
+		System.out.println("user2: "+usersRepository.getUser(id2));
+		
+		System.out.println("users: "+usersRepository.getAll());
+		
+		usersRepository.getUser(0);
 	}
 }
