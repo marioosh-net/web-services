842292095bd2bc6fd84cc8fca76106cd90727f84
marioosh <marioosh.net@gmail.com>
2012-03-15 15:58:09 +0100

    REST CRUD save

--- m5_cw1_rs_server/src/pl/szkolenie/service/Library.java
+++ m5_cw1_rs_server/src/pl/szkolenie/service/Library.java
@@ -1,15 +1,24 @@
 package pl.szkolenie.service;
 
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import pl.szkolenie.model.Book;
 
@@ -33,8 +42,43 @@ public class Library {
 	public Book getBook(@PathParam("id") long id) {
 		Book book = books.get(id);
 		if(book == null) {
-			throw new WebApplicationException();
+			throw new WebApplicationException(Response.Status.NOT_FOUND);
 		}
 		return book;
 	}
+	
+	@GET
+	public List<Book> getBooks() {
+		return new ArrayList<Book>(books.values());
+	}
+	
+	@POST
+	public Response add(@FormParam("author") String author, @FormParam("title") String title) {
+		Book book = new Book(title, author);
+		book.setId(lastId.getAndIncrement());
+		books.put(book.getId(), book);
+		return Response.created(URI.create(""+book.getId())).build();
+	}
+	
+	@Path("{id:\\d+}")
+	@DELETE
+	public Response remove(@PathParam("id") long id) {
+		books.remove(id);
+		return Response.ok().build();
+	}
+	
+	@Path("update")
+	// @PUT
+	@POST
+	public Response update(@FormParam("id") long id, @FormParam("author") String author, @FormParam("title") String title) {
+		if(books.containsKey(id)) {
+			Book book = books.get(id);
+			book.setAuthor(author);
+			book.setTitle(title);
+			return Response.noContent().build();
+		}
+		return Response.status(Status.CREATED).build();
+	}
+	
+	
 }
